e007309035c43658199ac24bb88a41283d652962
BeanMaster0790 <109036367+BeanMaster0790@users.noreply.github.com>
2024-10-11 10:39:41 +0100

    Add Debug
    Version Duck was missing its debug code so ive added them back in

---
+++ BeanMachine/Debug/DebugValue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeanMachine.Debug
+{
+	public class DebugValue
+	{
+		public string Name;
+
+		public int? IntValue;
+		public float? FloatValue;
+		public string StringValue;
+
+		public DebugValue(string name, ref int value)
+		{
+			this.Name = name;
+			this.IntValue = value;	
+		}
+
+		public DebugValue(string name, float value)
+		{
+			this.Name = name;
+			this.FloatValue = value;
+		}
+
+		public DebugValue(string name, ref string value)
+		{
+			this.Name = name;
+			this.StringValue = value;
+		}
+	}
+}
